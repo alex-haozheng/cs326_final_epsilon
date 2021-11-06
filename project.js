162e8d6c1a335d50194e96f2270841e5a559a569
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-06 01:12:04 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,39 @@
+
+
+async function getResults() {
+    let response = await fetch('/search',{
+        method: 'GET',
+    
+    });
+    if (response.ok) {
+        let JSON = await response.json();
+
+        helper(JSON, "berkshire", "breakfast");
+        helper(JSON, "berkshire", "lunch");
+        helper(JSON, "berkshire", "dinner");
+
+        helper(JSON, "hampshire", "breakfast");
+        helper(JSON, "hampshire", "lunch");
+        helper(JSON, "hampshire", "dinner");
+
+        helper(JSON, "franklin", "breakfast");
+        helper(JSON, "franklin", "lunch");
+        helper(JSON, "franklin", "dinner");
+
+        helper(JSON, "worcester", "breakfast");
+        helper(JSON, "worcester", "lunch");
+        helper(JSON, "worcester", "dinner");
+    } 
+    else {
+        alert("An error has occured.");
+    }
+}
+
+function helper(data, hall, time){
+    for(let i in data[hall][time]){
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = JSON.stringify(data[hall][time][i]);
+        uniquePage.document.getElementById(JSON.stringify(JSON.stringify(hall[0]) + time)).appendChild(newDiv);    
+    }
+
+}
