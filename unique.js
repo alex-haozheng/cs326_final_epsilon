19236b27da632c2982e8f9d33a239a3f1023fdeb
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-06 01:07:48 -0400

    Update unique.js

--- unique.js
+++ unique.js
@@ -1,17 +1,39 @@
 
 
-async function getFavorites() {
-    let u = document.getElementById("username").value;
+async function getUnique() {
     let response = await fetch('/unique/view',{
         method: 'GET',
-        body: u
-    })
-
+    
+    });
     if (response.ok) {
+        let JSON = await response.json();
 
+        helper(JSON, "berkshire", "breakfast");
+        helper(JSON, "berkshire", "lunch");
+        helper(JSON, "berkshire", "dinner");
+
+        helper(JSON, "hampshire", "breakfast");
+        helper(JSON, "hampshire", "lunch");
+        helper(JSON, "hampshire", "dinner");
+
+        helper(JSON, "franklin", "breakfast");
+        helper(JSON, "franklin", "lunch");
+        helper(JSON, "franklin", "dinner");
+
+        helper(JSON, "worcester", "breakfast");
+        helper(JSON, "worcester", "lunch");
+        helper(JSON, "worcester", "dinner");
     } 
     else {
         alert("An error has occured.");
     }
 }
 
+function helper(data, hall, time){
+    for(let i = 0; i < data[hall][time].length; ++i){
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = JSON.stringify(data[hall][time][i]);
+        uniquePage.document.getElementById(JSON.stringify(JSON.stringify(hall[0]) + time)).appendChild(newDiv);    
+    }
+
+}
