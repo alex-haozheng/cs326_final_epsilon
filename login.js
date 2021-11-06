8b930e5e2e440c22fbbb184ff0997d77b584a019
altang23 <89962619+altang23@users.noreply.github.com>
2021-11-06 01:43:44 -0400

    Add files via upload
    Slightly changed the html page, added the register function to login, added link onclick to the login button to bring to the profile.html page

---
+++ login.js
@@ -0,0 +1,24 @@
+'use strict';
+
+async function register() {
+    let u = document.getElementById("username").value;
+    let p = document.getElementById("password").value;
+    let response = await fetch('/register',{
+        method: 'POST',
+        body: JSON.stringify([u,p])
+    })
+
+    if (response.ok) {
+        alert("Account has been created successfully.");
+    } 
+    else {
+        alert("An error has occured.");
+    }
+}
+
+
+function initialize() {
+    document.getElementById("signup").addEventListener("click", register);
+}
+
+window.onload = initialize;
