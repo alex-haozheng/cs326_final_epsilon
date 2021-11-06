096cfcde0ce829fbf8ff781cd279a434bdb528e5
altang23 <89962619+altang23@users.noreply.github.com>
2021-11-06 18:06:57 -0400

    Add files via upload
    made sure to make an empty array for the users favorites upon account creation, fixed the delete function to work based off url

--- server.js
+++ server.js
@@ -1,48 +1,183 @@
 'use strict';
-// const express = require('express');
-
-import * as http from 'http';
-import * as url from 'url';
-import * as fs from 'fs';
-import express from 'express';
+const express = require('express');
+// import express from 'express';
 const app = express();
 
 app.use(express.json()); // lets you handle JSON input
 
 const port = 8080;
 
-const datastore = {};
+const datastore = {
+  "uniques":{
+      "berkshire": {
+          "breakfast": ["pancakes", "sausages", "eggs"],
+          "lunch": ["pizza", "burgers", "french fries"],
+          "dinner": ["fish", "spaghetti", "chicken pot pie"]
+      },
+      "franklin": {
+          "breakfast": ["waffles", "sausages", "eggs"],
+          "lunch": ["pasta", "burgers", "french fries"],
+          "dinner": ["pork", "spaghetti", "chicken pot pie"]
+      },
+      "hampshire": {
+          "breakfast": ["biscuits", "sausages", "eggs"],
+          "lunch": ["soup", "burgers", "french fries"],
+          "dinner": ["turkey", "spaghetti", "chicken pot pie"]
+      },
+      "worcester": {
+          "breakfast": ["danish", "sausages", "eggs"],
+          "lunch": ["ribs", "burgers", "french fries"],
+          "dinner": ["fish", "spaghetti", "chicken pot pie"]
+      }
+  
+  },
+
+  "logins": {
+      "user1": "pass1",
+      "user2": "pass2",
+      "user3": "pass3"
+  },
+
+  "profiles":{
+      "user1": ["fav1", "fav2"],
+      "user2": ["fav3", "fav4"],
+      "user3": ["fav5", "fav6"] 
+  },
+  
+  "food":{
+      "date1": {
+          "berkshire": {
+              "breakfast": {
+                  "name1": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": false    
+                  }
+              },
+              "lunch": {
+                  "name2": {
+                      "Halal": false,
+                      "Vegetarian": false,
+                      "Gluten Free": false    
+                  }
+              },
+              "dinner": {
+                  "name3": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              }
+          },
+          "franklin": {
+              "breakfast": {
+                  "name1": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "lunch": {
+                  "name2": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "dinner": {
+                  "name3": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              }
+          },
+          "hampshire": {
+              "breakfast": {
+                  "name1": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "lunch": {
+                  "name2": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "dinner": {
+                  "name3": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              }
+          },
+          "worcester": {
+              "breakfast": {
+                  "name1": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "lunch": {
+                  "name2": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              },
+              "dinner": {
+                  "name3": {
+                      "Halal": true,
+                      "Vegetarian": true,
+                      "Gluten Free": true    
+                  }
+              }
+          }
+  
+      }
+  }
+  
+};
+
 const JSONfile = 'storage.json';
 
-function reload(filename) {
-  if (fs.existsSync(filename)) {
-    return JSON.parse(fs.readFileSync(filename));
-  } else {
-    return {"wordScore": [], "gameScore": []};
-  }
+
+
+
+async function search() {
+  let berk = await berk();
+  let hamp = await hamp();
+  let woo = await woo();
+  let frank = await frank();
+
 }
 
+async function berk() {
 
-async function search(obj, arr) {
-  let o = JSON.parse(JSON.stringify(obj));
+}
 
-  for(let i = 0; i <= arr[1]; ++i) {
-    for (let hall in obj.food[i]) {
-      for (let meal in hall) {
-        let keys = Object.keys(meal);
-        let 
-      }
-    }
-  }
+async function hamp() {
+
+}
+
+async function woo() {
+
+}
+
+async function frank() {
+
 }
 
 // ['search', int, true, false, true]
 app.get('/search', async (req, res) => {
-  const o = JSON.parse(JSON.stringify())
+  const o = datastore.food
   
 });
-
-
 
 // req: {"username": "user1", "password": "pass1"}
 app.post('/register', (req, res) => {
@@ -50,6 +185,7 @@ app.post('/register', (req, res) => {
   let password = req.body.password;
   if(!datastore["logins"][username]){
     datastore["logins"][username] = password;
+    datstore["profiles"][username] = [];
   }
   res.send(res.statusCode);
 });
@@ -71,15 +207,11 @@ app.post('/user/favorites/add', (req, res) => {
   res.send(res.statusCode);
 });
 
-app.delete('/user/delete', (req, res) => {
-  // TODO: PARSE OUT KEY AND VALUE FROM req.body INTO k and v
-  let body = '';
-  req.on('data', data => body += data);
-  req.on('end', async () => {
-    delete datastore.logins[body];
-    delete datastore.profiles[body];
-  });
-  res.end();
+app.delete('/user/delete/:key', (req, res) => {
+  let user = req.params.key;
+  delete datastore["logins"][user];
+  delete datastore["profiles"][user];
+  res.send(res.statusCode);
 });
 
 app.listen(port, () => {
