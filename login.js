fb73a8833b3bb9ab13938d4b5ba85b1f7e1ab69a
sazzle2 <azheng.umass.edu>
2021-11-06 23:06:44 -0400

    main edits

--- login.js
+++ login.js
@@ -7,15 +7,13 @@ async function register() {
         method: 'POST',
         body: JSON.stringify({"username": u, "password": p})
     })
-
     if (response.ok) {
         alert("Account has been created successfully.");
-    } 
+    }
     else {
         alert("An error has occured.");
     }
 }
-
 
 function initialize() {
     document.getElementById("signup").addEventListener("click", register);
