13be31032bf62f75a9ad8f48142eccb655078472
sazzle2 <azheng@umass.edu>
2021-11-18 13:10:56 -0500

    removed persistency

--- server.js
+++ server.js
@@ -11,30 +11,71 @@ app.use(express.json()); // lets you handle JSON input
 
 const port = 8080;
 
-let datastore = {};
-const JSONfile = './storage.json';
+let datastore = {
+  "uniques":{
+     "berkshire": {
+         "breakfast": ["pancakes", "sausages", "eggs"],
+         "lunch": ["pizza", "burgers", "french fries"],
+         "dinner": ["fish", "spaghetti", "chicken pot pie"]
+     },
+     "franklin": {
+         "breakfast": ["waffles", "sausages", "eggs"],
+         "lunch": ["pasta", "burgers", "french fries"],
+         "dinner": ["pork", "spaghetti", "chicken pot pie"]
+     },
+     "hampshire": {
+         "breakfast": ["biscuits", "sausages", "eggs"],
+         "lunch": ["soup", "burgers", "french fries"],
+         "dinner": ["turkey", "spaghetti", "chicken pot pie"]
+     },
+     "worcester": {
+         "breakfast": ["danish", "sausages", "eggs"],
+         "lunch": ["ribs", "burgers", "french fries"],
+         "dinner": ["fish", "spaghetti", "chicken pot pie"]
+     }
+  },
 
-function reload(filename) {
-  if (fs.existsSync(filename)) {
-    return JSON.parse(fs.readFileSync(filename));
-  } else {
-    return {
-      unique: {},
-      logins: {},
-      profiles: {},
-      food: {}
-    };
+  "logins": {
+      "user1": "pass1",
+      "user2": "pass2",
+      "user3": "pass3"
+  },
+
+  "profiles":{
+      "user1": ["fav1", "fav2"],
+      "user2": ["fav3", "fav4"],
+      "user3": ["fav5", "fav6"] 
+  },
+  food: {
+    berkshire: {},
+    hampshire: {},
+    worcester: {},
+    franklin: {}
   }
-}
+};
+// const JSONfile = './storage.json';
+
+// function reload(filename) {
+//   if (fs.existsSync(filename)) {
+//     return JSON.parse(fs.readFileSync(filename));
+//   } else {
+//     return {
+//       unique: {},
+//       logins: {},
+//       profiles: {},
+//       food: {}
+//     };
+//   }
+// }
 
 app.get('/search', async (req, res) => {
-  datastore = reload(JSONfile);
+  // datastore = reload(JSONfile);
   res.send(datastore.food);
 });
 
 // req: {"username": "user1", "password": "pass1"}
 app.post('/register', (req, res) => {
-  datastore = reload(JSONfile); //reload function causing the pages to function weird
+  // datastore = reload(JSONfile); //reload function causing the pages to function weird
   let username = req.body.username;
   let password = req.body.password;
   if(!datastore["logins"][username]) {
@@ -46,15 +87,13 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/unique/view', (req, res) => {
-  datastore = reload(JSONfile);
+  // datastore = reload(JSONfile);
   res.send(datastore["uniques"]);
 });
 
 app.get('/user/favorites/view/:key', (req, res) => {
-  datastore = reload(JSONfile);
+  // datastore = reload(JSONfile);
   let username = req.params.key;
-  console.log(username);
-  console.log(datastore);
   // if(datastore.profiles[username] === undefined) {
   //   //make response be not ok / display error message
   //   res.end('no user exists');
@@ -65,8 +104,7 @@ app.get('/user/favorites/view/:key', (req, res) => {
 
 // req: {"username": "user1", "item": "chicken"}
 app.post('/user/favorites/add', (req, res) => {
-  datastore = reload(JSONfile);
-  console.log(req.body);
+  // datastore = reload(JSONfile);
   let username = req.body.username;
   let item = req.body.item;
   // if(datastore.profiles[username] === undefined) {
@@ -74,14 +112,13 @@ app.post('/user/favorites/add', (req, res) => {
   //   res.end('no user exists');
   // }
   datastore.profiles[username].push(item);
-  console.log(datastore.profiles[username]);
   fs.writeFileSync(JSONfile, JSON.stringify(datastore));
   res.end();
 });
 
 // should work 100% :)
 app.delete('/user/delete/:key', (req, res) => {
-  datastore = reload(JSONfile);
+  // datastore = reload(JSONfile);
   let user = req.params.key;
   delete datastore["logins"][user];
   delete datastore["profiles"][user];
