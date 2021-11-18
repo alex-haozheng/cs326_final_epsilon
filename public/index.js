cc7805d97840f5aa4546c9b286a80397fb046928
sazzle2 <azheng@umass.edu>
2021-11-18 13:20:22 -0500

    change index

--- public/index.js
+++ public/index.js
@@ -1,62 +1,124 @@
 'use strict';
 
-async function getFavorites() {
-    let response = await fetch('http://localhost:8080/user/favorites/view/' + document.getElementById("username").value,{
+/**
+ * send search info to server
+ */
+async function search(){
+    let response = await fetch('http://localhost:8080/search',{
         method: 'GET'
-    });
-    if (response.ok) {
-        let arr = await response.json(); // array of favorites
-        document.getElementById('favoriteList').innerHTML = '';
-        for(let i = 0; i < arr.length; i++) {
-            let newDiv = document.createElement('div');
-            newDiv.innerHTML = JSON.stringify(arr[i]);
-            document.getElementById("favoriteList").append(newDiv);
-        }
-    }
-    else {
-        alert("An error has occured.");
-    }
-}
-//test comment
-async function addFavorite() {
-    let g = JSON.stringify({username: document.getElementById("username").value, item: document.getElementById("adding").value});
-    let response = await fetch('http://localhost:8080/user/favorites/add', {
-        method: 'POST',
-        body: g,
-        headers: { 'Content-Type': 'application/json' }
-    });
+    })
 
     if (response.ok) {
-        let arr = await response.json(); // array of favorites
-        document.getElementById('favoriteList').innerHTML = '';
-        for(let i = 0; i < arr.length; i++) {
-            let newDiv = document.createElement('div');
-            newDiv.innerHTML = JSON.stringify(arr[i]);
-            document.getElementById("favoriteList").append(newDiv);
-        }
-    }
-    else {
-        alert("An error has occured.");
-    }
-}
-
-async function deleteAccount() {
-    let response = await fetch('http://localhost:8080/user/delete/' + document.getElementById("username").value, {
-        method: 'DELETE'
-    });
-
-    if (response.ok) {
-      alert("The account has been deleted");
+        const data = response.json();
+        return data;
     } 
     else {
         alert("An error has occured.");
     }
+    
+}
+//do later
+// function filter(data) {
+//   let copy = JSON.parse(JSON.stringify(data));
+//   console.log(copy);
+//   let keyword = document.getElementById("mySearch").value;
+//   let days = 1;
+//   let halal = document.getElementById("halal").checked;
+//   let vegetarian = document.getElementById("vegetarian").checked;
+//   let glutenFree = document.getElementById("gluten").checked;
+//   for(let i = 0; i <= days; ++i) {
+//     console.log(copy[i]);
+//     for (let hall in copy[i]) {
+
+//       for (let meal in hall) {
+//         for (let name in meal) {
+//           //console.log(hall);
+//           console.log(copy[i][hall]);  
+//           if (name.includes(keyword) && keyword !== '') {
+//             console.log(keyword);
+//             if(halal) {                         //checked halal
+//               console.log('checked hal');
+//               if(!name.hasOwnProperty("halal")) {                 //food is/not halal
+//                 console.log("not halal");
+//                 delete copy[hall][meal][name];
+//               }
+//             }
+//             if(vegetarian) {
+//               if(!name.hasOwnProperty("vegetarian")) {
+//                 console.log("not veg");
+
+//                 delete copy[hall][meal][name];
+//               }
+//             }
+//             if(glutenFree) {
+//               if(!name.hasOwnProperty("gluten free")) {
+//                 console.log("not glu");
+
+//                 delete copy[hall][meal][name];
+//               }
+//             }
+//           } else {
+//             console.log(name);
+//             console.log(copy[i][hall]);
+//           //  delete name;
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return copy;
+// }
+/**
+ * request results from server and display resultss
+ */
+async function getResults() {
+    let data = await search();
+    console.log(data);
+    let copy = data;
+    console.log(copy);
+   // filter(data);
+    console.log(typeof(data));
+    for(let i = 0; i < data.length; ++i){   //loop through each date and display its data
+            display(data[i], i, "berkshire");
+            display(data[i], i, "hampshire");
+            display(data[i], i, "franklin");
+            display(data[i], i, "worcester");
+
+    }
+    
 }
 
+/**
+ * display results of search
+ * @param {*} data the results array
+ * @param {*} date what date is being displayed
+ * @param {*} hall which hall card it is being displayed in
+ */
+function display(data, date, hall){
+        // console.log(date);
+        // console.log(data);
+        let dateDiv = document.createElement('div');
+      //  dateDiv.classList.add("list-group-item");
+        dateDiv.innerText = date;
+        document.getElementById(hall).appendChild(dateDiv);
 
+        for(let meal in data[hall]){                     //create a div for each meal, append to date div
+            let mealDiv = document.createElement('div');
+            mealDiv.innerHTML = meal;
+            dateDiv.appendChild(mealDiv);
 
-window.addEventListener('load', () => {
-  document.getElementById("search").addEventListener("click", addFavorite);
-  document.getElementById("delete").addEventListener("click", deleteAccount);
-  document.getElementById("getFav").addEventListener("click", getFavorites);
-});
+            for(let item in data[hall][meal]){                              //create a div for each item, append to meal div
+                let itemDiv = document.createElement('div');
+                itemDiv.innerHTML = item;
+                mealDiv.appendChild(itemDiv);
+            }
+    
+        }
+
+    
+};
+
+window.onload = initialize;
+function initialize(){
+    document.getElementById("searchBtn").addEventListener("click", getResults)
+}
