4e043b3153d8ddf5973d22b7ea4061df872b9d4f
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-19 20:57:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // import express from 'express';
 const app = express();
-const port = 3000;
+const port = 8080;
 
 let http = require('http');
 let url = require('url');
