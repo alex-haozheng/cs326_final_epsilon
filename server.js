2afaec8ea59c9fe630c37d74d21056a5a426d3c8
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-19 20:42:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(express.json()); // lets you handle JSON input
 
 
-app.get('/', (req, res) => res.send('its over!!'));
+//app.get('/', (req, res) => res.send('its over!!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
 // 'use strict';
