4d14db5e336a7b18d3274d1c4a3b654fcd7b31d8
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-19 20:40:32 -0500

    Add files via upload

---
+++ public/login.js
@@ -0,0 +1,23 @@
+'use strict';
+
+async function register() {
+    let response = await fetch('http://localhost:8080/register',{
+        method: 'POST',
+        body: JSON.stringify({"username": document.getElementById("username").value, "password": document.getElementById("password").value}),
+        headers: {'Content-Type': 'application/json' }
+    })
+
+    if (response.ok) {
+        alert("Account has been created successfully.");
+    } 
+    else {
+        alert("An error has occured.");
+    }
+}
+
+
+function initialize() {
+    document.getElementById("signup").addEventListener("click", register);
+}
+
+window.onload = initialize;
---
+++ public/profile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+async function getFavorites() {
+    let response = await fetch('http://localhost:8080/user/favorites/view/' + document.getElementById("username").value,{
+        method: 'GET'
+    });
+    if (response.ok) {
+        let arr = await response.json(); // array of favorites
+        document.getElementById('favoriteList').innerHTML = '';
+        for(let i = 0; i < arr.length; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = JSON.stringify(arr[i]);
+            document.getElementById("favoriteList").append(newDiv);
+        }
+    }
+    else {
+        alert("An error has occured.");
+    }
+}
+//test comment
+async function addFavorite() {
+    let g = JSON.stringify({username: document.getElementById("username").value, item: document.getElementById("adding").value});
+    let response = await fetch('http://localhost:8080/user/favorites/add', {
+        method: 'POST',
+        body: g,
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        let arr = await response.json(); // array of favorites
+        document.getElementById('favoriteList').innerHTML = '';
+        for(let i = 0; i < arr.length; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = JSON.stringify(arr[i]);
+            document.getElementById("favoriteList").append(newDiv);
+        }
+    }
+    else {
+        alert("An error has occured.");
+    }
+}
+
+async function deleteAccount() {
+    let response = await fetch('http://localhost:8080/user/delete/' + document.getElementById("username").value, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+      alert("The account has been deleted");
+    } 
+    else {
+        alert("An error has occured.");
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+  document.getElementById("search").addEventListener("click", addFavorite);
+  document.getElementById("delete").addEventListener("click", deleteAccount);
+  document.getElementById("getFav").addEventListener("click", getFavorites);
+});
