267c5f7934569cbf30d94fbd0452f1d8c7de9ec6
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-19 20:57:39 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@
  * send search info to server
  */
 async function search(){
-    let response = await fetch('http://localhost:8080/search',{
+    let response = await fetch('https://umassdining.herokuapp.com/search',{
         method: 'GET'
     })
 
