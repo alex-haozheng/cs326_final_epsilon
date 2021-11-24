7768d85b78afa3ac699064b02b2b574ff22a807d
altang23 <89962619+altang23@users.noreply.github.com>
2021-11-23 23:49:07 -0500

    Add files via upload
    fixed routing of getUnique function, made a preliminary incomplete function for getUnique, currently sorts the "today's" menu into each hall and each meal for each hall

--- public/unique.js
+++ public/unique.js
@@ -1,11 +1,71 @@
 'use strict';
 
 async function getUnique() {
-    let response = await fetch('http://localhost:8080/unique/view',{
+    let response = await fetch('/unique/view',{
         method: 'GET'
     });
     if (response.ok) {
         let out = await response.json();
+        //create variables that will be arrays for each menu at each dining hall
+        let bbreakfast = [];
+        let blunch = [];
+        let bdinner = [];
+        let hbreakfast = [];
+        let hlunch = [];
+        let hdinner = [];
+        let fbreakfast = [];
+        let flunch = [];
+        let fdinner = [];
+        let wbreakfast = [];
+        let wlunch = [];
+        let wdinner = [];
+        //out will be today's menu
+        // for(let item in out){
+        //     if(item[hall] === "Berkshire"){
+        //         if(item[meal] === "Breakfast"){
+        //             bbreakfast.push(item[name]);
+        //         }
+        //         if(item[meal] === "Lunch"){
+        //             blunch.push(item[name]);
+        //         }
+        //         if(item[meal] === "Dinner"){
+        //             bdinner.push(item[name]);
+        //         }
+        //     }
+        //     if(item[hall] === "Hampshire"){
+        //         if(item[meal] === "Breakfast"){
+        //             hbreakfast.push(item[name]);
+        //         }
+        //         if(item[meal] === "Lunch"){
+        //             hlunch.push(item[name]);
+        //         }
+        //         if(item[meal] === "Dinner"){
+        //             hdinner.push(item[name]);
+        //         }
+        //     }
+        //     if(item[hall] === "Franklin"){
+        //         if(item[meal] === "Breakfast"){
+        //             fbreakfast.push(item[name]);
+        //         }
+        //         if(item[meal] === "Lunch"){
+        //             flunch.push(item[name]);
+        //         }
+        //         if(item[meal] === "Dinner"){
+        //             fdinner.push(item[name]);
+        //         }
+        //     }
+        //     if(item[hall] === "Worcester"){
+        //         if(item[meal] === "Breakfast"){
+        //             wbreakfast.push(item[name]);
+        //         }
+        //         if(item[meal] === "Lunch"){
+        //             wlunch.push(item[name]);
+        //         }
+        //         if(item[meal] === "Dinner"){
+        //             wdinner.push(item[name]);
+        //         }
+        //     }
+        // }
 
         helper(out, "berkshire", "breakfast");
         helper(out, "berkshire", "lunch");
