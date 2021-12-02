fb97088cf317c7c26af6f1e09db0a0f85e70b711
sazzle2 <azheng@umass.edu>
2021-12-02 14:06:28 -0500

    fixed server for search

--- public/index.js
+++ public/index.js
@@ -11,26 +11,27 @@ async function search(){
         headers: { 'Content-Type': 'application/json'}
     })
     if (response.ok) {
-        const data = JSON.parse(response);
+        const data = await response.json();
+        console.log(data);
         for (let i = 0; i < data.length; i++){
             if(data[i]['hall'] === "Berkshire"){
                 let newDiv = document.createElement('div');
-                newDiv.innerHTML = 'item: ${JSON.stringify(data[i][name])} date: ${JSON.stringify(data[i][date])} meal: ${JSON.stringify(data[i][meal])}';
+                newDiv.innerHTML = `item: ${JSON.stringify(data[i]['name'])} date: ${JSON.stringify(data[i]['date'])} meal: ${JSON.stringify(data[i]['meal'])}`;
                 document.getElementById("berkshire").appendChild(newDiv);
             }
             if(data[i]['hall'] === "Hampshire"){
                 let newDiv = document.createElement('div');
-                newDiv.innerHTML = 'item: ${JSON.stringify(data[i][name])} date: ${JSON.stringify(data[i][date])} meal: ${JSON.stringify(data[i][meal])}';
+                newDiv.innerHTML = `item: ${JSON.stringify(data[i]['name'])} date: ${JSON.stringify(data[i]['date'])} meal: ${JSON.stringify(data[i]['meal'])}`;
                 document.getElementById("hampshire").appendChild(newDiv);
             }
             if(data[i]['hall'] === "Franklin"){
                 let newDiv = document.createElement('div');
-                newDiv.innerHTML = 'item: ${JSON.stringify(data[i][name])} date: ${JSON.stringify(data[i][date])} meal: ${JSON.stringify(data[i][meal])}';
+                newDiv.innerHTML = `item: ${JSON.stringify(data[i]['name'])} date: ${JSON.stringify(data[i]['date'])} meal: ${JSON.stringify(data[i]['meal'])}`;
                 document.getElementById("franklin").appendChild(newDiv);
             }
             if(data[i]['hall'] === "Worcester"){
                 let newDiv = document.createElement('div');
-                newDiv.innerHTML = 'item: ${JSON.stringify(data[i][name])} date: ${JSON.stringify(data[i][date])} meal: ${JSON.stringify(data[i][meal])}';
+                newDiv.innerHTML = `item: ${JSON.stringify(data[i]['name'])} date: ${JSON.stringify(data[i]['date'])} meal: ${JSON.stringify(data[i]['meal'])}`;
                 document.getElementById("worcester").appendChild(newDiv);
             }
         }
