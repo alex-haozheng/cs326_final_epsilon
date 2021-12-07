f8d69bbac78c5371b4beffba39b5a825ae88ddec
sazzle2 <azheng@umass.edu>
2021-12-07 13:13:00 -0500

    changes to unique

--- public/unique.js
+++ public/unique.js
@@ -232,27 +232,37 @@ function berkPic(){
     document.body.style.backgroundImage = "url('berkshire.jpg')";
     document.body.style.backgroundSize = "100% 610px";
     document.body.style.backgroundRepeat = "no-repeat";
+	document.getElementById('title1').style.color = 'white';
+	document.getElementById('title2').style.color = 'white';
 }
 
 function hampPic(){
     document.body.style.backgroundImage = "url('hampshire.png')";
     document.body.style.backgroundSize = "100% 610px";
     document.body.style.backgroundRepeat = "no-repeat";
+	document.getElementById('title1').style.color = 'white';
+	document.getElementById('title2').style.color = 'white';
 }
 
 function frankPic(){
     document.body.style.backgroundImage = "url('franklin.jpg')";
     document.body.style.backgroundSize = "100% 610px";
     document.body.style.backgroundRepeat = "no-repeat";
+	document.getElementById('title1').style.color = 'white';
+	document.getElementById('title2').style.color = 'white';
 }
 
 function wooPic(){
     document.body.style.backgroundImage = "url('worcester.jpg')";
     document.body.style.backgroundSize = "100% 610px";
     document.body.style.backgroundRepeat = "no-repeat";
+	document.getElementById('title1').style.color = 'white';
+	document.getElementById('title2').style.color = 'white';
 }
 function normBack(){
     document.body.style.background = "rgb(136,28,28)";
+	document.getElementById('title1').style.color = 'black';
+	document.getElementById('title2').style.color = 'black';
 }
 
 
